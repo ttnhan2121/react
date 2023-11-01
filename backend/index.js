49c97546fd8eda90bf2680ce09dd74c722f6ea57
ttnhan2121 <144778001+ttnhan2121@users.noreply.github.com>
2023-11-01 20:59:04 +0700

    db

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
 const app = express()
 const port = 3000
 
+// HTTP logger
+app.use(morgan('combined'))
+
 app.get('/home', (req, res) => {
-  res.send('Hello World!')
+  res.send('123 ')
 })
 
 app.listen(port, () => {
